3e8a38af98645ec3a8e77bc95c928df81288b877
Blake Caldwell <blakecaldwell123@gmail.com>
2020-06-04 14:18:28 -0700

    Added GraphDraw
    - User can now display dependency graphs focused on a parent file
    - Allow users to open multiple windows at once
    - Added CLI

--- core/Main.java
+++ core/Main.java
@@ -1,6 +1,7 @@
 package core;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
 
@@ -8,12 +9,17 @@ public class Main {
 
 	static String[] searchItems = {"include", "require"}; // the terms used in files to denote dependency
 	static Hashtable<String, ParentFile> listOfDepends;
-	
-	public static void main(String[] args) throws FileNotFoundException {
+	static int frameVert;
+	static int frameHorz;
+	static ArrayList<String> listOfDependents; 
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		
 		String path = "";
 		String[] filesInDir;
+		frameVert = 1000;
+		frameHorz = 1000;
 		listOfDepends = new Hashtable<String, ParentFile>();
-		
+		listOfDependents = new ArrayList<String>();		
 		
 		if(args.length > 0) {
 			path = args[0];
@@ -44,25 +50,52 @@ public class Main {
 					// if the line contained a dependency
 					if(newFile != null) {
 						// add dependency edge
-						addEdge(newFile, filename);
+						
+						// get just the name of the file, ignore paths
+						File tempFile = new File(newFile);
+						addEdge(tempFile.getName(), filename);
 					}
 
 					
 				}
+				fileReader.close();
 			}
 		}
+		
+				
 	
-		// print out results
+//		 print out results
+		System.out.println("List of Parents:");
 		for(ParentFile parent : listOfDepends.values()){
-			System.out.println("Parent: " + parent.getName());
-			System.out.print("Dependents:");
-			for(String name : parent.getDepends()) {
-				System.out.print(" " + name + " ");
-			}
-			System.out.println("\n");
+			System.out.println(parent.getName());
+//			System.out.print("Dependents:");
+//			for(String name : parent.getDepends()) {
+//				System.out.print(" " + name + " ");
+//			}
 		}
+		
+		boolean isTrue = true;
+		Scanner input = new Scanner(System.in);
+		while(isTrue) {
+			System.out.println("Type file name to see dependents");
+			System.out.print("Filename: ");
+			String response = input.nextLine();
+			// if input is a known file
+			if(listOfDepends.containsKey(response)) {
+				System.out.println("Showing " + response);
+				createGraph(response);
+			}else if(response.equalsIgnoreCase("exit")){
+				isTrue = false;
+				// exit all threads
+				
+			}else {
+				System.out.println("Unknown File!");
+			}
+		}
+		input.close();
 
 	}
+
 	
 	private static boolean checkExtension(String nameIn) {
 		if(nameIn.contains(".php")) {
@@ -108,5 +141,47 @@ public class Main {
 		// add the name of the file that this dependency is in
 		parent.add(child);
 	}
+	
+	/**
+	 * Create a graph of dependencies for the selected file
+	 * @param focusFile filename to create depenedency graph of
+	 */
+	private static void createGraph(String focusFile) {
+		GraphDraw frame = new GraphDraw("Test Window");
+		frame.setSize(frameHorz,frameVert);
+		
+		frame.setVisible(true);
+
+		
+		
+		//add parent to screen
+		frame.addNode(listOfDepends.get(focusFile).getName(), frameHorz/2, frameVert/2);
+		
+		int dependendSize = listOfDepends.get(focusFile).getDepends().size();
+		// radius of circle
+		int r = 400;
+		// add child nodes
+		for(int i=0;i<listOfDepends.get(focusFile).getDepends().size();i++){
+			String child = listOfDepends.get(focusFile).getDepends().get(i);
+			// add it to the list so it doesnt get repeated (for future use with multiple focus files
+			listOfDependents.add(child);
+			// add node
+			int x = (int) ((frameHorz/2) + r * Math.cos(2 * Math.PI * i / dependendSize));
+		    int y = (int) ((frameVert/2) + r * Math.sin(2 * Math.PI * i / dependendSize)); 
+		    int childSize;
+		    if(listOfDepends.containsKey(child)) {
+		    	childSize = listOfDepends.get(child).getDepends().size();
+		    }else {
+		    	childSize = 0;
+		    }
+			
+	    	frame.addNode(child + ", " + childSize, x, y);
+			//add edge
+			frame.addEdge(0,i+1);
+		}
+	
+	}
+	
+
 
 }
